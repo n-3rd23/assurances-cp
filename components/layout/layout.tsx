c8424e903a956038be0863f5ab1a23eed2068632
CodingXD <Marshallshelly97@gmail.com>
2020-12-07 19:59:58 +0530

    added router and basic seo

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,15 +1,36 @@
 import { NextSeo } from "next-seo";
+import { NextRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 import { Fragment } from "react";
 
 interface Props {
   title: string;
+  description: string;
   children: JSX.Element;
 }
 
-export default function Layout({ title, children }: Props) {
+export default function Layout({ title, description, children }: Props) {
+  const router: NextRouter = useRouter();
   return (
     <Fragment>
-      <NextSeo />
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={
+          router.pathname == "/"
+            ? "https://assurances.co.in"
+            : "https://assurances.co.in" + router.asPath
+        }
+        openGraph={{
+          type: "website",
+          url:
+            router.pathname == "/"
+              ? "https://assurances.co.in"
+              : "https://assurances.co.in" + router.asPath,
+          title: title,
+          description: description,
+        }}
+      />
       {children}
     </Fragment>
   );
