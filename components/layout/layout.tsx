f682ce3c96bb16ade58340ab1edb4b132dd35640
alan <alansaji>
2020-12-08 19:00:53 +0530

    finished creating layout component

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,7 +1,13 @@
+// sidebar bg -> background -> : #313541;
+// sidebar active_home background -> : #E95656;
+// sidebar icon_not active  -> background -> : #5A6371;
+
+
 import { NextSeo } from "next-seo";
 import { NextRouter } from "next/dist/client/router";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
+import styles from "./layout.module.scss"
 
 interface Props {
   title: string;
@@ -31,7 +37,31 @@ export default function Layout({ title, description, children }: Props) {
           description: description,
         }}
       />
-      {children}
+      {/* sidebar begins */}
+      <div className="container-fluid">
+       <div className="row">
+          <div className={`${styles.side_bar}`} >
+            <div className={`${styles.nav_item} ${styles.home} py-3`}>
+              <img className={styles.nav_icon} src="/icons/home.svg" alt="home"/>
+            </div>
+            <div className={`${styles.nav_item} py-3`}>
+              <img className={styles.nav_icon} src="/icons/bar-chart.svg" alt="home"/>
+            </div>
+            <div className={`${styles.nav_item} py-3`}>
+              <img className={styles.nav_icon} src="/icons/phone.svg" alt="home"/>
+            </div>
+            <div className={`${styles.nav_item} py-3`}>
+              <img className={styles.nav_icon} src="/icons/newspaper.svg" alt="home"/>
+            </div>
+            <div className={`${styles.nav_item} ${styles.logout} py-3`}>
+              <img className={styles.nav_icon} src="/icons/newspaper.svg" alt="home"/>
+            </div>
+          </div>
+          <div className="col">
+            {children}
+          </div>
+       </div>
+      </div>
     </Fragment>
   );
 }
---
+++ pages/admin/index.tsx
@@ -0,0 +1,11 @@
+import Layout from  "../../components/layout/layout"
+
+export default function Dashboard() {
+    return (
+        <Layout title={'dashboard'} description={'dashboard page of admin'}>
+            <div className="col-md-12">
+                hola
+            </div>
+        </Layout>
+    );
+}
