e760f810d327e25c5d84e6bf3f57a07cee1b65d4
SERVER-CODR <athulbabu007@depaul.edu.in>
2020-12-10 13:26:43 +0530

    completed features

---
+++ components/feature_card/feature_card.tsx
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import styles from "./feature_card.module.scss";
+import Safe from "../../public/icons/safe.svg";
+import Dove from "../../public/icons/dove.svg";
+import Bank from "../../public/icons/piggy-bank.svg";
+
+export default function Feature_Card({ icon, head, content }) {
+  return (
+    <div className={`container ${styles.containerStyle} text-center `}>
+      <div className="mt-4">
+        <div className={`${styles.roundedStyle} mx-auto`}>
+          {icon === "Safe" ? (
+            <Safe
+              className={`p-1 m-1 ${styles.iconStyle}`}
+              width={50}
+              height={50}
+            />
+          ) : icon === "Dove" ? (
+            <Dove className="p-1 m-1" width={50} height={50} />
+          ) : (
+            <Bank className="p-1 m-1" width={50} height={50} />
+          )}
+        </div>
+      </div>
+      <div className="mt-3 text-large text-primary fw-800">{head}</div>
+      <p className="mt-3 text-small px-5 py-2">{content}</p>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Hero from "../components/hero/hero";
 import Layout from "../components/layout/layout";
+import Bar from "../components/bar/bar";
+import Home_Features from "../components/home_features/home_features";
 
 export default function Home() {
   return (
     <Layout title="Assurances" description="Best Life Insurance ever">
       <Hero />
+      <Bar />
+      <Home_Features />
     </Layout>
   );
 }
