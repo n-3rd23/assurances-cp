b76ddcd23e12e2606c4bb7c4660e793750e37d84
SERVER-CODR <athulbabu007@depaul.edu.in>
2020-12-25 16:50:16 +0530

    insurance slug complteted

--- pages/insurance.tsx
+++ pages/insurance.tsx
@@ -1,46 +1,55 @@
 import Hero from "../components/hero/hero";
 import Layout from "../components/layout/layout";
 import UserPlanCard from "../components/userPlanCard/userPlanCard";
+import { firestore, storage } from "../firebase/firebase.util";
+import { message } from "antd";
+import { Component } from "react";
 
-export default function Insurance() {
-  return (
-    <Layout title="Assurances" description="Best Life Insurance ever">
-      <Hero subHead="FOR EVERY PURPOSE" mainHead="WE HAVE YOU COVERED" />
+export default class Insurance extends Component {
+  state = {
+    plans: {},
+  };
 
-      <div className="d-flex justify-content-around flex-wrap">
-        <div className="p-5">
-          {" "}
-          <UserPlanCard
-            subHead1="POLICY"
-            mainHead="Jeevan Anand"
-            subHead2="Secure your life with jeevan anand and enjoy it's benefits throughout your life.For you and you're family"
-          />
+  componentDidMount() {
+    this.getPlans();
+  }
+
+  getPlans = async () => {
+    let plansDb = {};
+    firestore
+      .collection("plans")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          plansDb[doc.id] = doc.data();
+        });
+      })
+      .then(() => this.setState({ plans: plansDb }))
+      .catch(function (error) {
+        message.error("Network issue encountered. Unable to fetch data.");
+      });
+  };
+
+  render() {
+    return (
+      <Layout title="Assurances" description="Best Life Insurance ever">
+        <Hero subHead="FOR EVERY PURPOSE" mainHead="WE HAVE YOU COVERED" />
+        <div className="d-flex justify-content-around flex-wrap">
+          {Object.entries(this.state.plans).map((item) => {
+            return (
+              <div className="p-5">
+                {" "}
+                <UserPlanCard
+                  subHead1="POLICY"
+                  mainHead={item[1].planName}
+                  subHead2={item[1].planSummary}
+                />
+              </div>
+            );
+            console.log(item[1]);
+          })}
         </div>
-        <div className="p-5">
-          {" "}
-          <UserPlanCard
-            subHead1="POLICY"
-            mainHead="Jeevan Anand"
-            subHead2="Secure your life with jeevan anand and enjoy it's benefits throughout your life.For you and you're family"
-          />
-        </div>
-        <div className="p-5">
-          {" "}
-          <UserPlanCard
-            subHead1="POLICY"
-            mainHead="Jeevan Anand"
-            subHead2="Secure your life with jeevan anand and enjoy it's benefits throughout your life.For you and you're family"
-          />
-        </div>
-        <div className="p-5">
-          {" "}
-          <UserPlanCard
-            subHead1="POLICY"
-            mainHead="Jeevan Anand"
-            subHead2="Secure your life with jeevan anand and enjoy it's benefits throughout your life.For you and you're family"
-          />
-        </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 }
--- pages/insurance/[slug].tsx
+++ pages/insurance/[slug].tsx
@@ -1,20 +1,122 @@
 import Layout from "../../components/layout/layout";
 import DetailsCard from "./../../components/detailsCard/detailsCard";
+import { Component } from "react";
+import { firestore } from "../../firebase/firebase.util";
+import { message } from "antd";
+import Router from "next/router";
+import Clock from "../../public/icons/clock.svg";
+import BenefitCard from "./../../components/benefitCard/benefitCard";
+import PlanHero from "./../../components/planHero/planHero";
 
-export default function Insurance() {
-  return (
-    <Layout title="Assurances" description="Best Life Insurance ever">
-      <div className="d-flex flex-wrap justify-content-center">
-        <DetailsCard subHead1="TERM" mainHead="15 - 30" subHead2="YEARS" />
-        <DetailsCard subHead1="TERM" mainHead="15 - 30" subHead2="YEARS" />
-        <DetailsCard subHead1="TERM" mainHead="15 - 30" subHead2="YEARS" />
-      </div>
-      <div className="container">
-        <p className="mt-5 fw-700" style={{ color: "#5a6371" }}>
-          SUMMARY
-        </p>
-        <p>test</p>
-      </div>
-    </Layout>
-  );
+export default class Insurance extends Component {
+  state = {
+    plans: {},
+    currentPlan: {},
+    term: {},
+    sum: {},
+    benefits: {},
+    premium: [],
+    images: {},
+  };
+
+  componentDidMount() {
+    this.getPlans();
+  }
+
+  getPlans = async () => {
+    let plansDb = {};
+    await firestore
+      .collection("plans")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          plansDb[doc.id] = doc.data();
+        });
+      })
+      .then(() =>
+        this.setState({ plans: plansDb }, this.findCurrentPlan(plansDb))
+      )
+      .catch(function (error) {
+        message.error("Network issue encountered. Unable to fetch data.");
+      });
+  };
+
+  findCurrentPlan = (plansDb) => {
+    Object.entries(plansDb).map((item) => {
+      item[1].planName == Router.query.title
+        ? this.setState({
+            currentPlan: item[1],
+            term: item[1].planTerm,
+            sum: item[1].sumAssured,
+            benefits: item[1].benefits,
+            premium: item[1].paymentMode,
+            images: item[1].images[0],
+          })
+        : "";
+    });
+  };
+
+  render() {
+    return (
+      <Layout title="Assurances" description="Best Life Insurance ever">
+        <PlanHero
+          subHead="Life is secure"
+          head={this.state.currentPlan.planName}
+          img={this.state.images.link}
+        />
+        <div className="d-flex flex-wrap justify-content-center">
+          <DetailsCard
+            subHead1="TERM"
+            mainHead={this.state.term.from + " - " + this.state.term.to}
+            subHead2="YEARS"
+          />
+          <DetailsCard
+            subHead1="MAX MATURITY"
+            mainHead={this.state.currentPlan.maxMaturityAge}
+            subHead2="YEARS"
+          />
+          <DetailsCard
+            subHead1="MIN SUM"
+            mainHead={this.state.sum.min}
+            subHead2="ASSURED"
+          />
+        </div>
+        <div className="container px-4 px-md-5 smTextCenter">
+          <a className="mt-5 fw-700" style={{ color: "#5a6371" }}>
+            SUMMARY
+          </a>
+          <p className="text-interSize">{this.state.currentPlan.planSummary}</p>
+        </div>
+
+        <div className="d-flex container mt-5 px-4 px-md-5 justify-contents-start">
+          <div className="p-2">
+            <Clock width={60} height={60} />
+          </div>
+          <div className="p-2">
+            <a className="text-medium fw-600" style={{ color: "#545e75" }}>
+              PREMIUM
+            </a>
+            <br />
+            {this.state.premium.map((item) => {
+              return <a className="fw-700 text-middle"> {item},</a>;
+            })}
+          </div>
+        </div>
+
+        <div className="container mb-5 px-4 px-md-5">
+          <p className="mt-5 fw-700" style={{ color: "#5a6371" }}>
+            BENEFITS
+          </p>
+          {Object.entries(this.state.benefits).map((item) => {
+            return (
+              <BenefitCard
+                head={item[1].title.toUpperCase()}
+                desc={item[1].description}
+              />
+            );
+          })}
+        </div>
+      </Layout>
+    );
+  }
 }
