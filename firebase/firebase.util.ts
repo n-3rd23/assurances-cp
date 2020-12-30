bcf6a2e9a6db3c1c288afae6626f0d3cf17f3366
CodingXD <marshallshelly97@gmail.com>
2020-12-30 23:23:04 +0530

    Added interface for Event

--- firebase/firebase.util.ts
+++ firebase/firebase.util.ts
@@ -25,7 +25,14 @@ export const pageview = (url) => {
   });
 };
 
-export const event = ({ action, category, label, value }) => {
+interface Event {
+  action: string;
+  category: string;
+  label: string;
+  value?: string;
+}
+
+export const event = ({ action, category, label, value }: Event) => {
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
