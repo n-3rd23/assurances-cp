64a98fb1f0f942c7f367a6982220e89cb683c333
Alan <alansaji23@hotmail.com>
2021-01-25 23:55:18 +0530

    added whatsapp icon

---
+++ components/whatsapp/whatsapp.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+
+export default function Whatsapp() {
+  useEffect(() => {
+    loadWhatsapp();
+  },[]);
+
+  const loadWhatsapp = () => {
+    var options = {
+      whatsapp: "+919846117476", // WhatsApp number
+      call_to_action: "Message us", // Call to action
+      position: "right", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol,
+      host = "getbutton.io",
+      url = proto + "//static." + host;
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = url + "/widget-send-button/js/init.js";
+    s.onload = function () {
+      WhWidgetSendButton.init(host, proto, options);
+    };
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(s, x);
+  };
+  return <div className="d-none"></div>;
+}
