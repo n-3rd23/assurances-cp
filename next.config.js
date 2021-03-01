09e4e27ac6db2abf5d35b38b2c226a2be9f24ed3
TheUltimateCookie <marshall@growth.cx>
2021-03-01 14:11:06 +0530

    Added sitemap generator

--- next.config.js
+++ next.config.js
@@ -2,19 +2,53 @@ const withPlugins = require("next-compose-plugins");
 const withReactSvg = require("next-react-svg");
 const path = require("path");
 
-module.exports = withPlugins([
-  [
-    withReactSvg,
-    {
-      include: path.resolve(__dirname, "./public"),
-      webpack(config, options) {
-        return config;
-      },
+// module.exports = withPlugins([
+//   [
+//     withReactSvg,
+//     {
+//       include: path.resolve(__dirname, "./public"),
+//       webpack(config, options) {
+//         return config;
+//       },
+//     },
+//   ],
+//   [
+//     {
+//       images: {
+//         domains: ["firebasestorage.googleapis.com"],
+//       },
+//     },
+//   ],
+//   [
+// {
+//   webpack: (config, { isServer }) => {
+//     if (isServer) {
+//       require("./utils/generateSiteMap");
+//     }
+
+//     return config;
+//   },
+// },
+//   ],
+// ]);
+
+(module.exports = withReactSvg({
+  include: path.resolve(__dirname, "./public"),
+  webpack(config, options) {
+    return config;
+  },
+})),
+  {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require("./utils/generateSiteMap");
+      }
+
+      return config;
     },
-  ],
+  },
   {
     images: {
       domains: ["firebasestorage.googleapis.com"],
     },
-  },
-]);
+  };
